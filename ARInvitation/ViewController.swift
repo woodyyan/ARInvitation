983870537c06ac283242e6ccc96ed30c3a0247c4
WoodyYan <sbyan@CNsbyan.local>
2016-10-29 22:20:55 +0800

    实现了追踪二维码。

--- ARInvitation/ViewController.swift
+++ ARInvitation/ViewController.swift
@@ -32,51 +32,58 @@ class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
         let  captureDeviceIntput : AVCaptureDeviceInput = try!  AVCaptureDeviceInput(device: cameraDevice)
         let captureOutput: AVCaptureMetadataOutput = AVCaptureMetadataOutput()
         let session = AVCaptureSession()
-        // 判断输入源能否添加到捕捉任务中
         if !session.canAddInput(captureDeviceIntput) {
             return
         }
-        // 判断输出源能否添加到捕捉任务中
         if !session.canAddOutput(captureOutput) {
             return
         }
         
-        //添加两个源
         session.addInput(captureDeviceIntput)
         session.addOutput(captureOutput)
         
-        //设置输出源的代理和队列
         captureOutput.setMetadataObjectsDelegate(self, queue:DispatchQueue.main)
         
-        //设置源数据类型  当前使用的是:二维码类型
         captureOutput.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
         
         viewpreviewLayer = AVCaptureVideoPreviewLayer(session: session)
-        
-        //设置图层大小
         viewpreviewLayer!.frame = view.frame
-        
         view.layer.insertSublayer(viewpreviewLayer!, at: 0)
         
-        //这句话忘了写什么都没有
         session.startRunning()
     }
     
-    //扫描时的数据代理
     func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [Any]!, from connection: AVCaptureConnection!) {
-        //判断转化为AVMMRCO后的数组的第一个对象是否为空，不为空赋值给message，message类型是否为AVMetadataObjectTypeQRCode(二维码类型)和meesage的字符串值不为nil
         if let message = metadataObjects.first as? AVMetadataMachineReadableCodeObject, message.type == AVMetadataObjectTypeQRCode && message.stringValue != nil{
             
-            if let _ = self.view.viewWithTag(101) {
-                
+            // 把坐标转换成为在layer层上面的真实坐标
+            let transformObj = self.viewpreviewLayer?.transformedMetadataObject(for: message)
+            let qrCodeObj = transformObj as! AVMetadataMachineReadableCodeObject
+            print(qrCodeObj.corners)
+            
+            let corner1 = qrCodeObj.corners.first
+            let corner2 = qrCodeObj.corners[1]
+            let corner3 = qrCodeObj.corners[2]
+            let upperLeftPoint = CGPoint(dictionaryRepresentation: corner1 as! CFDictionary)
+            let upperRightPoint = CGPoint(dictionaryRepresentation: corner2 as! CFDictionary)
+            let lowerRightPoint = CGPoint(dictionaryRepresentation: corner3 as! CFDictionary)
+            let width = (upperRightPoint!.x - upperLeftPoint!.x) + 100
+            let height = (upperRightPoint!.y - lowerRightPoint!.y) + 100
+            
+            print(upperLeftPoint)
+            
+            // stringValue: 二维码的具体内容
+            print(qrCodeObj.stringValue)
+            
+            if let playerView = self.view.viewWithTag(101) {
+                playerView.frame = CGRect(x: upperLeftPoint!.x, y: upperLeftPoint!.y, width: width, height: height)
             }else {
-                let playerView = PlayerView(frame: CGRect(x: 10, y: 10, width: 200, height: 200))
-                playerView.tag = 101
-                self.view.addSubview(playerView)
+                let redView = UIView(frame: CGRect(x: upperLeftPoint!.x, y: upperLeftPoint!.y, width: width, height: height))
+                redView.backgroundColor = UIColor.blue
+                //let playerView = PlayerView(frame: CGRect(x: 10, y: 10, width: 200, height: 200))
+                redView.tag = 101
+                self.view.addSubview(redView)
             }
-//            let alert = UIAlertController(title: "二维码内容", message: message.stringValue, preferredStyle: UIAlertControllerStyle.alert)
-//            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: nil))
-//            self.present(alert, animated: true, completion: nil)
         }
     }
 }
